7c141c6bc7b3b9ead4ba83b9f4e8542a19c6775b
TimooMF <RIG210335@spengergasse.at>
2025-11-05 15:15:42 +0100

    airline

---
+++ 2025-11-05_airline/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "model";
+import { faker, pl } from "@faker-js/faker";
+const prisma = new PrismaClient();
+
+const ensurePassengers = 20000;
+const ensureAirports = 100;
+const ensurePlanes = 250;
+
+// ensure passengers (no deps)
+while (await prisma.passenger.count() < ensurePassengers) {
+    try {
+        await prisma.passenger.create({
+            data: {
+                firstName: faker.person.firstName(),
+                lastName: faker.person.lastName(),
+                email: faker.internet.email(),
+            },
+        });
+    } catch (e) {
+        console.error(`Error creating passenger`, (e as Error).message);
+    }
+}
+// ensure planes (no deps)
+const planes_to_create = ensurePlanes - await prisma.plane.count();
+for (let i = 0; i < planes_to_create;) {
+    await prisma.plane.create({
+        data: {
+            model: faker.airline.airplane().name,
+            capacity: faker.number.int({ min: 10, max: 850 }),
+        },
+    });
+    i++;
+}
+
+// ensure 20 airports (no deps)
+// for (let i = 0; i < 10; i++) {
+//     const fake_airport = faker.airline.airport();
+//     await prisma.airport.create({
+//         data: {
+//             name: fake_airport.name,
+//             iataCode: fake_airport.iataCode,
+//             city: faker.location.city(),
+//         },
+//     });
+// }
+
+// ensure 20 fligths (depends on airport, plane)
